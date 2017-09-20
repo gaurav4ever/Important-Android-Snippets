f4fe1ff5564f7d92cbd57bec224d2345f1675fc6
Gaurav Sharma <gauravsharma.mvp@gmail.com>
2017-09-20 23:03:24 +0530

    code added

--- LocatinBasedService/app/src/main/java/com/example/gauravpc/locatinbasedservice/AllLocations.java
+++ LocatinBasedService/app/src/main/java/com/example/gauravpc/locatinbasedservice/AllLocations.java
@@ -1,6 +1,9 @@
 package com.example.gauravpc.locatinbasedservice;
 
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,6 +16,7 @@ import android.widget.Toast;
 public class AllLocations extends AppCompatActivity {
 
     private ProgressDialog pDialog;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +27,21 @@ public class AllLocations extends AppCompatActivity {
         pDialog.setMessage("Please wait...");
         pDialog.setCancelable(false);
 
-        favMealsListView=(ListView)findViewById(R.id.favMealsList);
+        listView=(ListView)findViewById(R.id.list);
         boolean check = isNetworkAvailable();
         if (check == true) {
-            getData(userid_val);
+            getData();
         } else {
             Toast.makeText(getApplicationContext(), "Please Check Your Internet Connection", Toast.LENGTH_SHORT).show();
         }
 
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager)getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 
 }
