47ba6f3756659b94affd5ca9851025b62ddb6183
Gaurav Sharma <gauravsharma.mvp@gmail.com>
2017-10-25 19:01:31 +0530

    cpp removede

--- MobileComputing/app/src/main/cpp/native-lib.cpp
+++ MobileComputing/app/src/main/cpp/native-lib.cpp
@@ -6,7 +6,8 @@ JNIEXPORT jstring JNICALL
 Java_com_example_gauravpc_mobilecomputing_MainActivity_stringFromJNI(JNIEnv* env, jobject jobj,jstring msg) {
 
     std::string hello = "Hello from C++";
-    hello.append((const char *) msg);
-
+//    hello.append((const char *) msg);
+    const char *path = env->GetStringUTFChars(msg , NULL ) ;
     return env->NewStringUTF(hello.c_str());
+//    return env->NewStringUTF(path);
 }
